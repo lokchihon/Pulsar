21d09590b06913c7a506de0d454d6a989c928267
jbaldjr98 <46031883+jbaldjr98@users.noreply.github.com>
2019-04-15 18:37:20 -0400

    Update Pulsar.py

--- Pulsar.py
+++ Pulsar.py
@@ -1,56 +1,19 @@
-
 class Pulsar:
 
-
-	def __init__(self, m = 42, lum=43, x=44, z=45, crr= 'RR', maxA=47, maxP= 48, temp= 49):
-		self.mass = m
-		self.luminosity = lum
-		self.xComp= x
-		self.zComp = z
-		self.cOrRR = crr
-		self.maxAmp = maxA
-		self.maxPeriodNum = maxP
-		self.temperature = temp
-
-
-	def getMass(self):
-		return self.mass
-
-	def getLum(self):
-		return self.luminosity
-
-	def getX(self):
-		return self.xComp
-
-	def getCRR(self):
-		return self.cOrRR
-
-	def getTemp(self):
-		return self.temp
-
-	def getMaxAmp(self):
-		return self.maxAmp
-
-	def getMaxPeriodNum(self):
-		return self.maxPeriodNum
-
-	def editRRFile(self, mass, lum, x,z, temp, maxA, maxP):
-		lines[7]={0,1,2,3,4,5,6}
-
-	def editCommonFile(self, mass, lum, x,z, temp, maxA, maxP):
-		lines[7] = {0, 1, 2, 3, 4, 5, 6}
-
-	def editCepheidFile(self, mass, lum, x,z, temp, maxA, maxP):
-		lines[7] = {0, 1, 2, 3, 4, 5, 6}
-		return lines
-
-	def main(self):
-		lines = editCepheidFile()
-		for x in lines:
-			print(x)
-	
+    def __init__(self, m, lum, x, z, crr, maxA, maxP, temp):
+        self.mass = m
+        self.luminosity = lum
+        self.xComp = x
+        self.zComp = z
+        self.cOrRR = crr
+        self.maxAmp = maxA
+        self.maxPeriodNum = maxP
+        self.temperature = temp
 
 
+pulsar1 = Pulsar(44, 201, 42, 42.3, "C", 45, 46, 47)  # sample pulsar that has nothing in common with a real one
 
-		
-
+if pulsar1.cOrRR == "RR":
+    print("RR")
+elif pulsar1.cOrRR == "C":
+    print("C")
